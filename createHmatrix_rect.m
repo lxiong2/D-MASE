7c15335c29e443fda2e1b13e928554e768df16ac
lxiong2 <lxiong7@VL515-SG02.ad.gatech.edu>
2015-04-23 14:55:03 -0400

    Modified createHmatrix functions
    Modified createHmatrix inner functions so that the size of the H matrix
    is the number of local measurements x the length of each local state
    vector. Modified realPowerInjMeas and reactivePowerInjMeas so that they
    only use local variables, including local G and B matrices. Not 100%
    sure if they're implemented correctly, especially for the sum across all
    buses. Now it's only accounting for buses that are connected to the bus
    of interest.

--- createHmatrix_rect.m
+++ createHmatrix_rect.m
@@ -16,10 +16,11 @@ numImeas = 0;
 % Angle
 numthmeas = 0;
 % measurement Jacobian H
-H = zeros(size(type,1),numbus*2);
+H = zeros(size(type,1),size(buses_a,1)*2);
 
 %% Determine type of measurement
 for a = 1:size(type,1)
+    a
     % Real power injection measurements [dPdth dPdV]
     if strcmp(type(a),'p') == 1
         numPmeas = numPmeas + 1;
@@ -36,31 +37,31 @@ for a = 1:size(type,1)
     elseif strcmp(type(a),'pf') == 1
         numPFmeas = numPFmeas + 1;
         indPFmeas = indices(a,:);
-        [dPijde, dPijdf] = realPowerFlowMeas_rect(e,f,G_a,B_a,numbus,buses_a,lines,indPFmeas);
+        [dPijde, dPijdf] = realPowerFlowMeas_rect(e,f,G_a,B_a,buses_a,indPFmeas);
         H(a,:) = [dPijde dPijdf];
     % Reactive power flow measurements [dQijdth dQijdV]   
     elseif strcmp(type(a),'qf') == 1
         numQFmeas = numQFmeas + 1;
         indQFmeas = indices(a,:);
-        [dQijde, dQijdf] = reactivePowerFlowMeas_rect(e,f,G_a,B_a,numbus,buses_a,lines,indQFmeas);
+        [dQijde, dQijdf] = reactivePowerFlowMeas_rect(e,f,G_a,B_a,buses_a,lines,indQFmeas);
         H(a,:) = [dQijde dQijdf];
-    % Voltage magnitude measurements    
-    elseif strcmp(type(a),'v') == 1
-        numVmeas = numVmeas + 1;
-        indVmeas = indices(a,:);
-        [dV2de, dV2df] = vMeas_rect(e,f,numbus,buses,buses_a,indVmeas);
-        H(a,:) = [dV2de dV2df];
-%     % Current magnitude measurements
-    %FIX: NOT DEBUGGED OR TESTED
-%     elseif strcmp(type(a),'i') == 1
-%         numImeas = numImeas + 1;
-%         indImeas = indices(a,:);  
-%         [dIijdth dIijdV] = iMeas(theta,V,I,numbus,buses,lines,numImeas,indImeas);
-%         H(a,:) = [dIijdth dIijdV];
-    elseif strcmp(type(a),'th') == 1
-        numthmeas = numthmeas + 1;
-        indthmeas = indices(a,:);
-        [dth2de, dth2df] = thMeas_rect(e,f,numbus,buses,buses_a,indthmeas);
-        H(a,:) = [dth2de dth2df];
+%     % Voltage magnitude measurements    
+%     elseif strcmp(type(a),'v') == 1
+%         numVmeas = numVmeas + 1;
+%         indVmeas = indices(a,:);
+%         [dV2de, dV2df] = vMeas_rect(e,f,numbus,buses,buses_a,indVmeas);
+%         H(a,:) = [dV2de dV2df];
+% %     % Current magnitude measurements
+%     %FIX: NOT DEBUGGED OR TESTED
+% %     elseif strcmp(type(a),'i') == 1
+% %         numImeas = numImeas + 1;
+% %         indImeas = indices(a,:);  
+% %         [dIijdth dIijdV] = iMeas(theta,V,I,numbus,buses,lines,numImeas,indImeas);
+% %         H(a,:) = [dIijdth dIijdV];
+%     elseif strcmp(type(a),'th') == 1
+%         numthmeas = numthmeas + 1;
+%         indthmeas = indices(a,:);
+%         [dth2de, dth2df] = thMeas_rect(e,f,numbus,buses,buses_a,indthmeas);
+%         H(a,:) = [dth2de dth2df];
     end
 end
--- realPowerInjMeas_rect.m
+++ realPowerInjMeas_rect.m
@@ -2,24 +2,26 @@ function [dPde, dPdf] = realPowerInjMeas_rect(e,f,G,B,numbus,buses,buses_a,adjbu
 % Elements of the measurement Jacobian H corresponding to
 % real power injection measurements
 
-dPde = zeros(1,numbus);
-dPdf = zeros(1,numbus);
+dPde = zeros(1,size(buses_a,1));
+dPdf = zeros(1,size(buses_a,1));
 busIndex = (1:numbus).';
 busIndex_a = (1:size(buses_a,1)).';
 
-for n = 1:numbus
+for a = 1:size(buses_a,1)
+    m_global = busIndex(buses==indPmeas(1,1));
     m = busIndex_a(buses_a==indPmeas(1,1));
-    if m == n
+    if m == a
         dPde(1,m) = G(m,m)*e(m)+B(m,m)*f(m);
-        for a = 1:numbus
-            dPde(1,m) = dPde(1,m)+(G(m,a)*e(a)-B(m,a)*f(a));
-        end
         dPdf(1,m) = -B(m,m)*e(m)+G(m,m)*f(m);
-        for b = 1:numbus
-            dPdf(1,m) = dPdf(1,m)+(G(m,b)*f(b)+B(m,b)*e(b));
+        temp = adjbuses(m_global,:); % get list of adjacent buses for bus m
+        temp = temp(temp~=0); % remove padded zeros
+        for b = 1:size(temp,2) % go through each adjacent bus
+            n = busIndex_a(buses_a == temp(b));
+            dPde(1,m) = dPde(1,m)+(G(m,n)*e(n)-B(m,n)*f(n));
+            dPdf(1,m) = dPdf(1,m)+(G(m,n)*f(n)+B(m,n)*e(n));
         end
     else
-        dPde(1,n) = G(m,n)*e(m)+B(m,n)*f(m);
-        dPdf(1,n) = -B(m,n)*e(m)+G(m,n)*f(m);
+        dPde(1,a) = G(m,a)*e(m)+B(m,a)*f(m);
+        dPdf(1,a) = -B(m,a)*e(m)+G(m,a)*f(m);
     end
 end
