9e57b31ad0812ce517f4c26ab429a5da0ef0aae5
lxiong2 <lxiong7@VL515-SG02.ad.gatech.edu>
2015-05-05 11:32:26 -0400

    Central Partition 2 results now match overall system results.
    Central Partition 2 results now match overall central system results.
    Original error was due to an error in the way x was mapped to f. Check
    that in the future if there are errors for other partitions.

--- test_14bus_smallerSE2.m
+++ test_14bus_smallerSE2.m
@@ -11,15 +11,15 @@ maxiter = 10;
 example_14bus_IEEE_partitions
 
 % flat start for AC
-x(:,1) = [ones(numbus3,1); zeros(numbus3-1,1)];
+x(:,1) = [ones(numbus2,1); zeros(numbus2-1,1)];
 deltax(:,1) = ones(size(x,1),1);
 
 numlines = size(lines3,1);
 lineStatus = repmat({'Closed'},[numlines 1]);
 
 %YBus_14AC
-%YBus_14AC_Part2
-YBus_14AC_Part3
+YBus_14AC_Part2
+%YBus_14AC_Part3
 %Ybus2 = calcYbus(buses, lines(:,1), lines(:,2), lines(:,4), lines(:,5), lines(:,6), lineStatus);
 G = real(Ybus);
 B = imag(Ybus);
@@ -36,29 +36,30 @@ B = imag(Ybus);
 
 while (norm(deltax(:,k)) > 1e-4) && (k < maxiter)
     % Rectangular AC version
-    e = x(1:numbus3,k);
-    f = [x(numbus3+1:(numbus3+slackIndex3-1)); 0; x((numbus3+slackIndex3):(2*numbus3-1),k)];
+    x
+    e = x(1:numbus2,k)
+    f = [x(numbus2+1:(numbus2+slackIndex2-1),k); 0; x((numbus2+slackIndex2):(2*numbus2-1),k)]
 
     %h(:,k) = zeros(size(z,1),1);
     % Form the measurement function h(x^k)
-    h(:,k) = createhvector_rect(e,f,G,B,alltype3,allindices3,numbus3,allbuses3,lines3); % rectangular
-    r(:,k) = allz3-h(:,k);
-    J(k) = (allz3-h(:,k)).'*(allR3\(allz3-h(:,k)));
+    h(:,k) = createhvector_rect(e,f,G,B,alltype2,allindices2,numbus2,allbuses2,lines2); % rectangular
+    r(:,k) = allz2-h(:,k);
+    J(k) = (allz2-h(:,k)).'*(allR2\(allz2-h(:,k)));
     
     % Form measurement Jacobian H
     % FIX: Test and debug iMeas.m
     % WARNING: Assumed gsi = 0 in realPowerFlowMeas.m
-    temp = createHmatrix_rect(e,f,G,B,alltype3,allindices3,numbus3,allbuses3,lines3);
+    temp = createHmatrix_rect(e,f,G,B,alltype2,allindices2,numbus2,allbuses2,lines2);
     
     % only slackIndex+1:(numbus) instead of slackIndex+1:(2*numbus) when
     % we're looking only at DC
-    H(:,:,k) = [temp(:,1:numbus3) temp(:,(numbus3+1):(numbus3+slackIndex3-1)) temp(:,(numbus3+slackIndex3+1):2*numbus3)];
+    H(:,:,k) = [temp(:,1:numbus2) temp(:,(numbus2+1):(numbus2+slackIndex2-1)) temp(:,(numbus2+slackIndex2+1):2*numbus2)];
 
     % Calculate gain matrix G(x^k) = H.'*R^(-1)*H
-    Gain(:,:,k) = H(:,:,k).'*(allR3\H(:,:,k));
+    Gain(:,:,k) = H(:,:,k).'*(allR2\H(:,:,k));
    
     % Compute right-hand side
-    rhs(:,k) = H(:,:,k).'*(allR3\(allz3-h(:,k)));
+    rhs(:,k) = H(:,:,k).'*(allR2\(allz2-h(:,k)));
     
     % Solve for dx
     deltax(:,k+1) = Gain(:,:,k)\rhs(:,k);
@@ -72,11 +73,11 @@ x
 k
 
 % Convert rectangular state variables to polar form
-newe = x(1:numbus3,k);
-newf = [x(numbus3+1:(numbus3+slackIndex3-1),k); 0; x((numbus3+slackIndex3):(2*numbus3-1),k)];
-newth = zeros(numbus3,1);
-newV = zeros(numbus3,1);
-for a = 1:numbus3
+newe = x(1:numbus2,k);
+newf = [x(numbus2+1:(numbus2+slackIndex2-1),k); 0; x((numbus2+slackIndex2):(2*numbus2-1),k)];
+newth = zeros(numbus2,1);
+newV = zeros(numbus2,1);
+for a = 1:numbus2
     newV(a) = sqrt(newe(a)^2+newf(a)^2);
     newth(a) = atan(newf(a)/newe(a));
 end
